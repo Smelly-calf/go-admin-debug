e75f026f37ab9928fa6caaef23dbdf3c795c9dbc
王倩 <wangqian445@jd.com>
2020-11-22 19:43:52 +0800

    add

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/GoAdminGroup/go-admin/engine"
+	"github.com/GoAdminGroup/go-admin/template"
+	"github.com/GoAdminGroup/go-admin/template/chartjs"
+	"github.com/GoAdminGroup/go-admin/tests/tables"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+
+	_ "github.com/GoAdminGroup/go-admin/adapter/gin"              // web framework adapter
+	_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/mysql" // sql driver
+	_ "github.com/GoAdminGroup/themes/adminlte"                   // ui theme
+)
+
+func main() {
+	startServer()
+}
+
+func startServer() {
+	gin.SetMode(gin.ReleaseMode)
+	gin.DefaultWriter = ioutil.Discard
+
+	r := gin.Default()
+
+	template.AddComp(chartjs.NewChart())
+
+	eng := engine.Default()
+
+	if err := eng.AddConfigFromJSON("./config.json").
+		AddGenerators(tables.Generators).
+		Use(r)
+		err != nil {
+		panic(err)
+	}
+
+	r.Static("/uploads", "./uploads")
+
+	_ = r.Run(":9033")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Print("closing database connection")
+	eng.MysqlConnection().Close()
+}
+
+
+
